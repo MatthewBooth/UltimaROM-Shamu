21ecf836a3718281f41cee2e59a14c06804aee32
Kryten2k35 <Kryten2k35@gmail.com>
2015-06-30 12:00:41 +0100

    Update base to LYZ28E

--- system/bin/install-recovery.sh
+++ system/bin/install-recovery.sh
@@ -1,6 +1,6 @@
 #!/system/bin/sh
-if ! applypatch -c EMMC:/dev/block/platform/msm_sdcc.1/by-name/recovery:8521004:f0cc7fb27f717c6513e7a57dc7b885755d915f47; then
-  applypatch -b /system/etc/recovery-resource.dat EMMC:/dev/block/platform/msm_sdcc.1/by-name/boot:7916839:f4e3e0a6455209e08569f5570c8e2a838d53f725 EMMC:/dev/block/platform/msm_sdcc.1/by-name/recovery f0cc7fb27f717c6513e7a57dc7b885755d915f47 8521004 f4e3e0a6455209e08569f5570c8e2a838d53f725:/system/recovery-from-boot.p && log -t recovery "Installing new recovery image: succeeded" || log -t recovery "Installing new recovery image: failed"
+if ! applypatch -c EMMC:/dev/block/platform/msm_sdcc.1/by-name/recovery:8533292:e44df71fe484286312cc62825e73892b7c4c8418; then
+  applypatch -b /system/etc/recovery-resource.dat EMMC:/dev/block/platform/msm_sdcc.1/by-name/boot:7927079:5b9d4ef0497ee50d3bc123711a590eae3ea78b6b EMMC:/dev/block/platform/msm_sdcc.1/by-name/recovery e44df71fe484286312cc62825e73892b7c4c8418 8533292 5b9d4ef0497ee50d3bc123711a590eae3ea78b6b:/system/recovery-from-boot.p && log -t recovery "Installing new recovery image: succeeded" || log -t recovery "Installing new recovery image: failed"
 else
   log -t recovery "Recovery image already installed"
 fi
